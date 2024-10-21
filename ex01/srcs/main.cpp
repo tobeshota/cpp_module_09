94fe488df7eb89ce6bb223b71fabbd9416873ca8
tobeshota <Shota.Tobe@keio.jp>
2024-10-21 22:56:22 +0900

    feat: argument check executes in rpn();

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -1,11 +1,7 @@
 #include "RPN.hpp"
 
-static bool isArgValid(int argc, char **argv) {
-  return (argc == 2 && argv[1][0] != '\0');
-}
-
 int main(int argc, char **argv) {
-  if (isArgValid(argc, argv) == false)
+  if (argc != 2)
     std::cerr << "Usage: ./RPN \"1 2 * 2 / 2 * 2 4 - +\"" << std::endl;
   else
     rpn(std::string(argv[1]));

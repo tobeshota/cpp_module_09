7955465751fe2333fdbe9999b1d72322907311d9
tobeshota <Shota.Tobe@keio.jp>
2024-10-21 22:44:37 +0900

    chore: rename test

--- ex01/test/srcs/RPNOKTest.cpp
+++ ex01/test/srcs/RPNOKTest.cpp
@@ -10,28 +10,28 @@ static std::string getRPNStdout(const std::string& input) {
 }
 
 // 加算
-TEST(OK, AdditionTest) {
+TEST(OK, Addition) {
   EXPECT_EQ(getRPNStdout("3 4 +"), "7\n");
   EXPECT_EQ(getRPNStdout("0 0 +"), "0\n");
   EXPECT_EQ(getRPNStdout("9 9 9 + +"), "27\n");
 }
 
 // 減算
-TEST(OK, SubtractionTest) {
+TEST(OK, Subtraction) {
   EXPECT_EQ(getRPNStdout("9 3 -"), "6\n");
   EXPECT_EQ(getRPNStdout("0 9 -"), "-9\n");
   EXPECT_EQ(getRPNStdout("9 9 0 - -"), "0\n");
 }
 
 // 乗算
-TEST(OK, MultiplicationTest) {
+TEST(OK, Multiplication) {
   EXPECT_EQ(getRPNStdout("6 7 *"), "42\n");
   EXPECT_EQ(getRPNStdout("0 0 *"), "0\n");
   EXPECT_EQ(getRPNStdout("4 4 4 * *"), "64\n");
 }
 
 // 徐算
-TEST(OK, DivisionTest) {
+TEST(OK, Division) {
   EXPECT_EQ(getRPNStdout("8 2 /"), "4\n");
   EXPECT_EQ(getRPNStdout("1 2 /"), "0\n");
   EXPECT_EQ(getRPNStdout("9 3 3 / /"), "9\n");

d9dc40107b0e5331a6e75fe2fd650af33b3630f0
tobeshota <Shota.Tobe@keio.jp>
2024-10-28 23:16:01 +0900

    fix: fix conditional expression of main

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,7 +1,7 @@
 #include "BitcoinExchange.hpp"
 
 int main(int argc, char **argv) {
-  if (argc != 2)
+  if (argc == 1)
     std::cerr << std::string(RED) + "Error: could not open file." +
                      std::string(DEFAULT) + "\n";
   else {
